08ed11260e4cc99c56ac9d5b1daf956d3368c872
joetime <joetime@gmail.com>
2016-09-20 14:59:46 -0400

    refactor - bk > BK

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -1,24 +1,18 @@
-import {
-  Component
-} from '@angular/core';
-import {
-  NavController
-} from 'ionic-angular';
-import {
-  Backand
-} from '../../services/backand';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Backand } from '../../services/backand';
 
 @Component({
   templateUrl: 'build/pages/page1/page1.html'
 })
 export class Page1 {
 
-  private bk: Backand;
+  private BK: Backand;
   public items: any[];
 
   constructor(public navCtrl: NavController, public backand: Backand) {
-    this.bk = backand;
-    this.bk.getTodos().subscribe(
+    this.BK = backand;
+    this.BK.getTodos().subscribe(
       data => {
         console.log("subscribe", data);
         this.items = data.data;
@@ -30,7 +24,7 @@ export class Page1 {
 
   public addTodo() {
     console.log('addTodo()')
-    this.bk.addTodo('item ' + new Date().toTimeString()).subscribe(
+    this.BK.addTodo('item ' + new Date().toTimeString()).subscribe(
       resp => console.log(resp),
       err => console.error(err)
     );
