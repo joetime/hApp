778cd2c0697cfaf6127aaf55bce2800124942e16
joetime <joetime@gmail.com>
2016-09-28 16:26:06 -0400

    photos

--- app/components/item-form/item-form.directive.ts
+++ app/components/item-form/item-form.directive.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Directive } from '@angular/core';
 import { PavingItem } from '../../models/item.model';
+import { CameraService } from '../../services/camera.service';
+import { Toast } from '../../services/toast.service';
 
 @Component({
     //moduleId: 'someId',
@@ -8,15 +10,38 @@ import { PavingItem } from '../../models/item.model';
 })
 export class ItemForm {
 
+    constructor(
+        private camera:CameraService, 
+        private T: Toast) {
+
+        console.info('ItemForm constructor')
+    }
+
     types = ['ADA', 'Repair', 'Maintenance', 'Other']
     model = new PavingItem(1, "Test Item", false, "ADA", "comments...");
     submitted = false;
 
     OnSubmit () { this.submitted = true; }
 
-    constructor() {
-        console.info('ItemForm constructor')
+    
+
+    public thumbnail_Click(ev) {
+
+        ev.target.src = "//placehold.it/100x100/ffcc22";
+        
+        this.camera.getPicture().then((imageData) => {
+            console.log(ev);
+            ev.target.src = 'data:image/jpeg;base64,' + imageData;
+
+        }, (err) => {
+
+            if (err == "no image selected") return; // not really an error
+
+            this.T.toast('Error accessing camera: ' + err);
+            ev.target.src = "//placehold.it/100x100/ffcc22";
+        })
     }
+
 
     get diagnostic() { return JSON.stringify(this.model); }
 
---
+++ app/services/comm.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+// The purpose of this class is to sync state across multiple components.
+// It maintains subscriptions to events, and fires off items when appropriate
+
+@Injectable()
+export class CommService {
+    
+    constructor() {}
+
+    
+
+}
